74d17b37046b6700986ff282e8431c4bbda039cf
Moa Granberg <moa.granberg@hotmail.com>
2020-03-13 15:15:17 +0100

    Revert "bugfix more than two closest paths"
    This reverts commit d67c0060894b10fe435399b2f9f17cedc2ce5167.

--- server/serverBoard.js
+++ server/serverBoard.js
@@ -49,14 +49,8 @@ class Board {
                         newPath.push(neighbours[0]);
                         paths.push(newPath);
                         path.push(neighbours[1]);
-                    } else if (neighbours.length === 3) {
-                        let firstNewPath = path.slice();
-                        let secondNewPath = path.slice();
-                        firstNewPath.push(neighbours[0]);
-                        secondNewPath.push(neighbours[1]);
-                        path.push(neighbours[2]);
-                        paths.push(firstNewPath);
-                        paths.push(secondNewPath);
+                    } else if (neighbours.length > 2) {
+                        console.log('error: fanns fler än två möjliga håll att gå från en position');
                     }
                     if (path.includes(start)) {
                         break
