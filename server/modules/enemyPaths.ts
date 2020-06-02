56452f498ef05a5076185ca8ce64ad4bb8619386
Moa Granberg <moa.granberg@hotmail.com>
2020-06-02 17:49:08 +0200

    add server-folder

---
+++ server/modules/enemyPaths.ts
@@ -0,0 +1,17 @@
+import positions from './serverPositions';
+
+const enemyPaths = [
+    [positions[8], positions[7], positions[11], positions[10], positions[14], positions[15], positions[16], positions[12], positions[8]],
+
+    [positions[8], positions[4], positions[3], positions[2], positions[6], positions[5], positions[9], positions[13]],
+
+    [positions[13], positions[9], positions[5], positions[6]],
+
+    [positions[6], positions[7], positions[8]],
+
+    [positions[13], positions[14], positions[15], positions[16], positions[12], positions[8]],
+
+    [positions[6], positions[2], positions[1], positions[5], positions[9], positions[13]],
+];
+
+export default enemyPaths;
