0dc2ceb2fc60d3003be163af347bf0b9763cf62e
Moa Granberg <moa.granberg@hotmail.com>
2020-09-11 22:00:42 +0200

    Style PaceButtons enemy & player
    Instructions for Choose pace, Caught & Waiting
    Button coloring
    Removal of enemyDisabled & playerDisabled

--- src/components/Board/UserActions/PaceButtons/PaceButtons.tsx
+++ src/components/Board/UserActions/PaceButtons/PaceButtons.tsx
@@ -12,19 +12,11 @@ interface PaceButtonsProps {
 
 const PaceButtons = ({ myPlayer, playersTurn, caught }: PaceButtonsProps) => {
   const [selectedPace, setSelectedPace] = useState<string>('');
-  const [enemyDisabled, setEnemyDisabled] = useState<boolean>(true);
-  const [playerDisabled, setPlayerDisabled] = useState<boolean>(true);
   const [hover, setHover] = useState<string>('');
-
-  useEffect(() => {
-    setEnemyDisabled(playersTurn);
-    setPlayerDisabled(!playersTurn);
-  }, [playersTurn]);
 
   useEffect(() => {
     if (!myPlayer.isEvil && caught && !selectedPace) {
       setSelectedPace('walk');
-      setPlayerDisabled(true);
 
       const params: OnPlayerSelectsPace = { pace: 'walk' };
       socket.emit('player selects pace', params);
@@ -42,10 +34,9 @@ const PaceButtons = ({ myPlayer, playersTurn, caught }: PaceButtonsProps) => {
     setSelectedPace(pace);
   };
 
-  const Button = ({ disabled, text }: { disabled: boolean; text: string }) => {
+  const Button = ({ text }: { text: string }) => {
     return (
       <button
-        disabled={disabled}
         className={
           selectedPace && selectedPace === text.toLowerCase() ? `${styles.button} ${styles.active}` : styles.button
         }
@@ -58,10 +49,35 @@ const PaceButtons = ({ myPlayer, playersTurn, caught }: PaceButtonsProps) => {
   };
 
   if (myPlayer.isEvil) {
+    if (playersTurn) {
+      return <h1>Waiting for your turn</h1>;
+    }
+
     return (
       <>
-        <Button disabled={enemyDisabled} text="Walk" />
-        <Button disabled={enemyDisabled} text="Run" />
+        <h1>Choose your pace</h1>
+
+        <div>
+          <Button text="Walk" />
+          <Button text="Run" />
+        </div>
+
+        {hover === 'walk' && (
+          <p>When walking, you can move 3-4 steps and listen after players at the end of your turn</p>
+        )}
+        {hover === 'run' && (
+          <p>
+            When running, you can move 5-6 steps and but <span>won't</span> listen for players
+          </p>
+        )}
+        {hover === '' && (
+          <p>
+            <br></br>
+            <br></br>
+          </p>
+        )}
+
+        {selectedPace && <p>Click on position next to your player to take a step</p>}
       </>
     );
   }
@@ -75,15 +91,21 @@ const PaceButtons = ({ myPlayer, playersTurn, caught }: PaceButtonsProps) => {
     );
   }
 
+  if (!playersTurn) {
+    return <h1>Waiting for your turn</h1>;
+  }
+
   return (
     <>
       <h1>Choose your pace</h1>
+
       <div>
-        <Button disabled={playerDisabled} text="Stand" />
-        <Button disabled={playerDisabled} text="Sneak" />
-        <Button disabled={playerDisabled} text="Walk" />
-        <Button disabled={playerDisabled} text="Run" />
+        <Button text="Stand" />
+        <Button text="Sneak" />
+        <Button text="Walk" />
+        <Button text="Run" />
       </div>
+
       {hover === 'stand' && <p>When standing, you can move 0 steps and makes noise up to 3 spaces away</p>}
       {hover === 'sneak' && <p>When sneaking, you can move 2 steps and makes noise up to 4 spaces away</p>}
       {hover === 'walk' && <p>When walking, you can move 3 steps and makes noise up to 5 spaces away</p>}
@@ -94,6 +116,7 @@ const PaceButtons = ({ myPlayer, playersTurn, caught }: PaceButtonsProps) => {
           <br></br>
         </p>
       )}
+
       {selectedPace && <p>Click on position next to your player to take a step</p>}
     </>
   );
