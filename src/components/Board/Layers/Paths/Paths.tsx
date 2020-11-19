cb1af86171f6e4f20dc4358cb58c2e7912f29795
moa-granberg <54362217+moa-granberg@users.noreply.github.com>
2020-11-19 09:11:01 +0100

    If enemy shares same path, it's purple (#56)
    * Refact Paths getContent-function with multiple returns for readability
    
    * If both enemies share same path, render one path-svg in enemy-common-color

--- src/components/Board/Layers/Paths/Paths.tsx
+++ src/components/Board/Layers/Paths/Paths.tsx
@@ -15,27 +15,31 @@ const Paths = ({ actionState, possiblePaths, viewAllPaths, e1Path, e2Path }: Pat
   const Enemy1PathComp = paths[getPathComponentName(e1Path)];
   const Enemy2PathComp = paths[getPathComponentName(e2Path)];
 
-  return (
-    <article className="enemy-paths-wrapper">
-      {actionState.key === 'select new path' ? (
-        possiblePaths.map((pathName) => {
-          const Comp = paths[getPathComponentName(pathName)];
-          return <Comp className={styles.path} key={pathName} />;
-        })
-      ) : viewAllPaths ? (
-        <>
-          {Object.values(paths).map((Path, index) => (
-            <Path className={styles.path} key={index} />
-          ))}
-        </>
-      ) : (
-        <>
-          <Enemy1PathComp className={styles.enemy1Path} />
-          <Enemy2PathComp className={styles.enemy2Path} />
-        </>
-      )}
-    </article>
-  );
+  const getContent = () => {
+    if (actionState.key === 'select new path') {
+      return possiblePaths.map((pathName) => {
+        const Comp = paths[getPathComponentName(pathName)];
+        return <Comp className={styles.path} key={pathName} />;
+      });
+    }
+
+    if (viewAllPaths) {
+      return Object.values(paths).map((Path, index) => <Path className={styles.path} key={index} />);
+    }
+
+    if (getPathComponentName(e1Path) === getPathComponentName(e2Path)) {
+      return <Enemy1PathComp className={styles.commonPath} />;
+    }
+
+    return (
+      <>
+        <Enemy1PathComp className={styles.enemy1Path} />
+        <Enemy2PathComp className={styles.enemy2Path} />
+      </>
+    );
+  };
+
+  return <article className="enemy-paths-wrapper">{getContent()}</article>;
 };
 
 const getPathComponentName = (str: string) => {
